d20a9a3319d19adf86e5fa2e51af93693878a929
rhos0129 <rhos0129@gmail.com>
2023-11-30 10:59:16 +0900

    S04 실전 예제 1 - 요구사항 분석과 기본 매핑

---
+++ src/main/java/org/example/jpashopv1/domain/Order.java
@@ -0,0 +1,53 @@
+package org.example.jpashopv1.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS") // Order가 예약어로 걸려있는 DB도 있다.
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/org/example/jpashopv1/domain/OrderStatus.java
@@ -0,0 +1,6 @@
+package org.example.jpashopv1.domain;
+
+public enum OrderStatus {
+
+    ORDER, CANCEL
+}
